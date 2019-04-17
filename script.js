f9d40aee8536eb9616551389e2991bce2ee58747
Ewa Niezgoda <eniezgo@gmail.com>
2019-04-17 17:58:24 +0200

    17.04

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ spn.addEventListener('click', () => {
     menu.classList.toggle('active');
 
 })
+
+const basic = document.querySelector('.basic');
+const premium = document.querySelector('.premium');
+const gold = document.querySelector('.gold');
+console.log(basic, premium, gold);
+
+let active = true;
+
+
+basic.addEventListener('click', () => {
+    active = true;
+    if (active === true) {
+        basic.classList.add('box-active');
+        premium.classList.add('box-inactive');
+        gold.classList.add('box-inactive');
+    }
+})
